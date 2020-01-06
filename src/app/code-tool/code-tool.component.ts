9a727c6ef9ee4e258845e6817eb3f3d743ed7462
sixcorners <sixcorners@gmail.com>
2020-01-06 02:04:29 -0600

    ng generate @angular/material:nav code-tool

---
+++ src/app/code-tool/code-tool.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-code-tool',
+  templateUrl: './code-tool.component.html',
+  styleUrls: ['./code-tool.component.scss']
+})
+export class CodeToolComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
