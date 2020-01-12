7d4f078f014042cd85dde09216ef8b5adf337e7a
sixcorners <sixcorners@gmail.com>
2020-01-11 21:30:30 -0600

    ng generate component pizza-tool

---
+++ src/app/pizza-tool/pizza-tool.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaToolComponent } from './pizza-tool.component';
+
+describe('PizzaToolComponent', () => {
+  let component: PizzaToolComponent;
+  let fixture: ComponentFixture<PizzaToolComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PizzaToolComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PizzaToolComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pizza-tool/pizza-tool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-tool',
+  templateUrl: './pizza-tool.component.html',
+  styleUrls: ['./pizza-tool.component.scss']
+})
+export class PizzaToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
