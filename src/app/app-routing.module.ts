9f61b8bd2d76d74f4fd7edd21fb7ab2ae2034dfd
sixcorners <sixcorners@gmail.com>
2020-01-11 21:38:56 -0600

    Set up routing using room component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoomGuard } from './room/room.guard';
+import { RoomComponent } from './room/room.component';
 import { CodeToolComponent } from './code-tool/code-tool.component';
+import { PizzaToolComponent } from './pizza-tool/pizza-tool.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'code' },
-  { path: 'code', pathMatch: 'full', canActivate: [RoomGuard], component: CodeToolComponent },
-  { path: 'code/:id', component: CodeToolComponent }
+  { path: 'code', pathMatch: 'full', canActivate: [RoomGuard], children: [] },
+  { path: 'code/:id', component: RoomComponent, children: [{
+    path: '',
+    component: CodeToolComponent
+  }]},
+  { path: 'pizza', pathMatch: 'full', canActivate: [RoomGuard], children: [] },
+  { path: 'pizza/:id', component: RoomComponent, children: [{
+    path: '',
+    component: PizzaToolComponent
+  }]},
 ];
 
 @NgModule({
