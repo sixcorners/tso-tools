de9ba62af4edf58248c6177b90f2aa856b04f906
sixcorners <sixcorners@gmail.com>
2021-06-30 09:10:27 -0500

    Wire up about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { PizzaToolComponent } from './pizza-tool/pizza-tool.component';
 import { CodeTopComponent } from './code-tool/code-top/code-top.component';
 import { PizzaSideComponent } from './pizza-tool/pizza-side/pizza-side.component';
 import { PizzaTopComponent } from './pizza-tool/pizza-top/pizza-top.component';
+import { AboutComponent } from './about/about.component';
+import { AboutSideComponent } from './about/about-side/about-side.component';
+import { AboutTopComponent } from './about/about-top/about-top.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'code' },
@@ -22,6 +25,12 @@ const routes: Routes = [
     { path: '', component: PizzaToolComponent },
     { path: '', component: PizzaSideComponent, outlet: 'side' },
     { path: '', component: PizzaTopComponent, outlet: 'top' },
+  ]},
+  { path: 'about', pathMatch: 'full', canActivate: [RoomGuard], children: [] },
+  { path: 'about/:room', component: RoomComponent, children: [
+    { path: '', component: AboutComponent },
+    { path: '', component: AboutSideComponent, outlet: 'side' },
+    { path: '', component: AboutTopComponent, outlet: 'top' },
   ]},
 ];
 
