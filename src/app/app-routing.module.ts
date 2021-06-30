e7f00a98297a2f59f760b24c90a06414f5be60ae
sixcorners <sixcorners@gmail.com>
2021-06-30 08:17:01 -0500

    Move top of page into new code-top component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RoomComponent } from './room/room.component';
 import { CodeToolComponent } from './code-tool/code-tool.component';
 import { CodeSideComponent } from './code-tool/code-side/code-side.component';
 import { PizzaToolComponent } from './pizza-tool/pizza-tool.component';
+import { CodeTopComponent } from './code-tool/code-top/code-top.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'code' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'code/:room', component: RoomComponent, children: [
     { path: '', component: CodeToolComponent },
     { path: '', component: CodeSideComponent, outlet: 'side' },
+    { path: '', component: CodeTopComponent, outlet: 'top' },
   ]},
   { path: 'pizza', pathMatch: 'full', canActivate: [RoomGuard], children: [] },
   { path: 'pizza/:room', component: RoomComponent, children: [
