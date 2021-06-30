93a8f31c03890c309a6ceeed79d1dd28925ce720
sixcorners <sixcorners@gmail.com>
2021-06-30 09:08:41 -0500

    npm run ng -- generate component about/about-side

---
+++ src/app/about/about-side/about-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-side',
+  templateUrl: './about-side.component.html',
+  styleUrls: ['./about-side.component.scss']
+})
+export class AboutSideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
