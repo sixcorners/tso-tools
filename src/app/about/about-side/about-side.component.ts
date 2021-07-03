a10045adec92cabdd0a27a21695e96c90aa782ba
sixcorners <sixcorners@gmail.com>
2021-07-03 01:53:57 -0500

    Copy top and side code into pizza-tool

--- src/app/about/about-side/about-side.component.ts
+++ src/app/about/about-side/about-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/room/room.service';
 
 @Component({
   selector: 'app-about-side',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-side.component.scss']
 })
 export class AboutSideComponent implements OnInit {
-
-  constructor() { }
+  private lastTimestamp = -Number.MAX_VALUE;
+  private lastRoomName: string;
+  readonly history = [];
+  constructor(room: RoomService) {
+    room.addEventListener('message', ({ data }) => {
+      if (this.lastRoomName != room.name) {
+        this.lastRoomName = room.name;
+        this.lastTimestamp = -Number.MAX_VALUE;
+        this.history.length = 0;
+      }
+      data = JSON.parse(data);
+      if (this.lastTimestamp >= data.timestamp)
+        return;
+      if (!data.message)
+        return;
+      this.lastTimestamp = data.timestamp;
+      this.history.push(data);
+      if (this.history.length >= 120)
+        this.history.shift();
+    });
+  }
 
   ngOnInit(): void {
   }
