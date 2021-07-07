1007bcbe8dceec7a957102d09017f8a3e3a6fe12
sixcorners <sixcorners@gmail.com>
2021-07-07 06:54:40 -0500

    Put all event listeners into singleton services

--- src/app/code-tool/code-side/code-side.component.ts
+++ src/app/code-tool/code-side/code-side.component.ts
@@ -8,36 +8,7 @@ import { ChartNavigatorService } from '../chart-navigator.service';
   styleUrls: ['./code-side.component.scss']
 })
 export class CodeSideComponent implements OnInit {
-  private lastTimestamp = -Number.MAX_VALUE;
-  private lastRoomName?: string;
-  readonly history: any[] = [];
-  constructor(private room: RoomService, private navigator: ChartNavigatorService) {
-    room.addEventListener('message', async ({ data }) => {
-      if (this.lastRoomName != room.name) {
-        this.lastRoomName = room.name;
-        this.lastTimestamp = -Number.MAX_VALUE;
-        this.history.length = 0;
-        this.navigator.reset();
-      }
-      data = JSON.parse(data);
-      if (this.lastTimestamp >= data.timestamp)
-        return;
-      if (!data.message)
-        return;
-      this.lastTimestamp = data.timestamp;
-      {
-        let match = data.message.match(/!moveNode (\d+)/);
-        if (match) {
-          await this.navigator.moveNode(+match[1]);
-          data.node = this.navigator.current.node;
-          data.parsed = `Moved to ${data.node.combination}`;
-        }
-      }
-      this.history.push(data);
-      if (this.history.length >= 120)
-        this.history.shift();
-    });
-  }
+  constructor(private room: RoomService, readonly navigator: ChartNavigatorService) { }
 
   ngOnInit(): void {
   }
--- src/app/pizza-tool/pizza-model.service.ts
+++ src/app/pizza-tool/pizza-model.service.ts
@@ -16,10 +16,48 @@ export class PizzaModelService {
       lastSelectionIngredient?: string,
     }
   } = {};
+  private lastTimestamp = -Number.MAX_VALUE;
+  private lastRoomName?: string;
+  readonly history: any[] = [];
 
   constructor(private room: RoomService) {
     for (let role of this.roles)
       this.model[role] = {};
+
+    room.addEventListener('message', ({ data }) => {
+      if (this.lastRoomName != room.name) {
+        this.lastRoomName = room.name;
+        this.lastTimestamp = -Number.MAX_VALUE;
+        this.history.length = 0;
+      }
+      data = JSON.parse(data);
+      if (this.lastTimestamp >= data.timestamp)
+        return;
+      if (!data.message)
+        return;
+      this.lastTimestamp = data.timestamp;
+      {
+        let match = data.message.match(/!ingredients (.+?) (..) (..) (..)/);
+        if (match) {
+          let role = this.model[match[1]];
+          for (let i of [1, 2, 3] as const)
+            role[i] = match[i + 1];
+          data.parsed = `${match[1]} has ${match[2]} ${match[3]} ${match[4]}`;
+        }
+      }
+      {
+        let match = data.message.match(/!bake (\d) (\d) (\d) (\d)/);
+        if (match) {
+          for (let i = 0; i < 4; i++)
+            this.model[this.roles[i]].selection = match[i + 1];
+          this.bake();
+          data.parsed = `Bake ${match[1]} ${match[2]} ${match[3]} ${match[4]}`;
+        }
+      }
+      this.history.push(data);
+      if (this.history.length >= 120)
+        this.history.shift();
+    });
   }
 
   bake() {
