ddd06ae4aec696c098949c1eb218131c2da98045
sixcorners <sixcorners@gmail.com>
2021-07-10 18:56:36 -0500

    Add info section

--- src/app/code-tool/code-tool.component.ts
+++ src/app/code-tool/code-tool.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RoomService } from '../room/room.service';
 import { ChartNavigatorService } from './chart-navigator.service';
 
@@ -8,10 +8,17 @@ import { ChartNavigatorService } from './chart-navigator.service';
   styleUrls: ['./code-tool.component.scss']
 })
 export class CodeToolComponent {
+  @ViewChild('info') info!: ElementRef<HTMLDivElement>;
+  @ViewChild('chart') chart!: ElementRef<HTMLDivElement>;
+
   constructor(readonly navigator: ChartNavigatorService, private room: RoomService) { }
 
   moveNode(node: any) {
     if (!node) return;
     this.room.sendMessage(`!moveNode ${node.id}`);
+  }
+
+  showHideInfo() {
+    setTimeout(() => this.info.nativeElement.style.display = this.chart.nativeElement.offsetLeft ? 'block' : '', 0);
   }
 }
