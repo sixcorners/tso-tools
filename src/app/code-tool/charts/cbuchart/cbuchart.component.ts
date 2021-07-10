4128978ac40bb5428ceb690601bede468d8e9edc
sixcorners <sixcorners@gmail.com>
2021-07-10 14:18:47 -0500

    Pass current into each chart component

--- src/app/code-tool/charts/cbuchart/cbuchart.component.ts
+++ src/app/code-tool/charts/cbuchart/cbuchart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cbuchart',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cbuchart.component.scss']
 })
 export class CbuchartComponent implements OnInit {
+  @Input() current: any;
 
   constructor() { }
 
--- src/app/code-tool/charts/twiddler-mim-code2/twiddler-mim-code2.component.ts
+++ src/app/code-tool/charts/twiddler-mim-code2/twiddler-mim-code2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-twiddler-mim-code2',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./twiddler-mim-code2.component.scss']
 })
 export class TwiddlerMimCode2Component implements OnInit {
+  @Input() current: any;
 
   constructor() { }
 
