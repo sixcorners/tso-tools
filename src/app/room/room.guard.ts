db3ad684f56c73991379d8e9ce4a202a42047d29
sixcorners <sixcorners@gmail.com>
2022-05-08 11:54:34 -0500

    Generate room id using nanoid

--- src/app/room/room.guard.ts
+++ src/app/room/room.guard.ts
@@ -1,38 +1,22 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router
 } from '@angular/router';
+import { nanoid } from 'nanoid/async';
 
 // https://juristr.com/blog/2018/11/better-route-guard-redirects/
 @Injectable({
   providedIn: 'root'
 })
 export class RoomGuard implements CanActivate {
-  constructor(private router: Router, private snackBar: MatSnackBar) { }
+  constructor(private router: Router) { }
 
   async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    let room = 'offline';
-    try {
-      let response = await fetch('/api/room', { method: 'POST' });
-      if (response.ok)
-        room = await response.text();
-      else {
-        console.error('server returned an error:', response);
-        this.snackBar.open(`server returned an error: ${await response.text()}`, 'OK');
-      }
-    } catch (error) {
-      console.error('failed to connect to server:', error);
-      let message = error;
-      if (error instanceof Error)
-        ({ message } = error);
-      this.snackBar.open(`failed to connect to server: ${message}`, 'OK');
-    }
     return this.router.createUrlTree(
-      [getResolvedUrl(next) + '/' + room]
+      [getResolvedUrl(next) + '/' + await nanoid()]
     );
   }
 }
