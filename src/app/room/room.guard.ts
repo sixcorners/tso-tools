299339e69eb28dd0017675ee307da278e5f9163d
sixcorners <sixcorners@gmail.com>
2023-12-18 18:12:15 -0600

    Use smaller ids generated from the alphabet derived from grc's ppp

--- src/app/room/room.guard.ts
+++ src/app/room/room.guard.ts
@@ -1,6 +1,10 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
-import { nanoid } from 'nanoid';
+import { customAlphabet } from 'nanoid';
+
+// https://www.grc.com/ppp.htm
+// https://zelark.github.io/nano-id-cc/
+const nanoid = customAlphabet('-_23456789:ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz', 7);
 
 // https://juristr.com/blog/2018/11/better-route-guard-redirects/
 export const roomGuard: CanActivateFn = (route) => {
