9634b20d0409c348b0dd264d46f69962d8d932f1
sixcorners <sixcorners@gmail.com>
2023-12-19 07:02:53 -0600

    Set MAT_SNACK_BAR_DEFAULT_OPTIONS to 2500

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -9,5 +10,5 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })]
+    }), {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}]
 };
