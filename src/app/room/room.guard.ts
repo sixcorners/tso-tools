7c50f473f16bfefc7d482970dbc6f8b444e481d5
sixcorners <sixcorners@gmail.com>
2023-12-18 18:18:13 -0600

    Simplify getResolvedUrl()

--- src/app/room/room.guard.ts
+++ src/app/room/room.guard.ts
@@ -5,13 +5,13 @@ import { nanoid } from 'nanoid';
 // https://juristr.com/blog/2018/11/better-route-guard-redirects/
 export const roomGuard: CanActivateFn = (route) => {
   return inject(Router).createUrlTree(
-    [getResolvedUrl(route) + '/' + nanoid()]
+    [...getResolvedUrl(route), nanoid()]
   );
 };
 
 // https://stackoverflow.com/a/53429547/988346
-function getResolvedUrl(route: ActivatedRouteSnapshot): string {
+function getResolvedUrl(route: ActivatedRouteSnapshot): string[] {
   return route.pathFromRoot
-    .map(v => v.url.map(segment => segment.toString()).join('/'))
-    .join('/');
+    .flatMap(v => v.url)
+    .map(String);
 }
