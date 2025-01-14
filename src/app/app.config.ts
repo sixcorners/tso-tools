2309e959e4ea4b513a93518570f01450d789ac88
sixcorners <sixcorners@gmail.com>
2025-01-14 16:30:39 -0500

    Deploy to Github Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterFeatures, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,9 +7,15 @@ import { provideServiceWorker } from '@angular/service-worker';
 
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
+const routerFeatures: RouterFeatures[] = [];
+
+// switch from the History API / pushState navigation if the base url is fully relative
+const base = document.querySelector('base')?.getAttribute('href');
+if (!base?.startsWith('/') && !base?.includes('://')) routerFeatures.push(withHashLocation());
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, ...routerFeatures), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000'
+  }), { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } }]
 };
